b7cdbb653d3f0cdca87952d6a75bc112eaa74bc2
Dswayam25 <dswayam0@gmail.com>
2026-03-19 14:59:14 +0530

    Nutri_AI.iml

--- app.py
+++ app.py
@@ -3,19 +3,17 @@ import google.generativeai as genai
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAI
 
-genai.configure(api_key='AIzaSyD7VFEhltOsCDi5GJLMqNIyqDWg0g2Julo')
+genai.configure(api_key='AIzaSyBXaGco-tmTgTZx9fwIhAWSgiXWES6dd7I')
 model = genai.list_models()
 for model in model:
     print(model)
-llm = GoogleGenerativeAI(model="models/gemini-2.5-flash", google_api_key='AIzaSyD7VFEhltOsCDi5GJLMqNIyqDWg0g2Julo',
+llm = GoogleGenerativeAI(model="models/gemini-2.5-flash", google_api_key='AIzaSyBXaGco-tmTgTZx9fwIhAWSgiXWES6dd7I',
                          temperature=1.0)
 nutritional_info_template = PromptTemplate(
     input_variables=["food_items"],
-    prompt_template="""provide detailed nutritionale following food items: {food_items}.
-    Include macronutrients (protein, fat, carbohydrates), micronutrients(vitamins, minerals), and calorie content."""
+    template="""Provide detailed nutritional information for the following food items: {food_items}.
+Include macronutrients (protein, fat, carbohydrates), micronutrients (vitamins, minerals), and calorie content."""
 )
-
-
 def get_food_items_input():
     with st.form("food_items_input_form"):
          food_items = st.text_area("Enter food items (separate by commas):", "")
@@ -29,12 +27,14 @@ def get_nutritional_info_response(input_data):
     if input_data is None:
       return "Error: No food items provided."
     prompt = nutritional_info_template.format(**input_data)
-    response = llm(prompt)
+    response = llm.invoke(prompt)
     return response
 st.title("NutriAI - Instant Nutritional Information")
+response = None
 input_data = get_food_items_input()
 if input_data:
     with st.spinner("Fetching Nutritional Information..."):
         response = get_nutritional_info_response(input_data)
 st.subheader("Nutritional Information")
-st.write(response)
+if response:
+   st.write(response)
